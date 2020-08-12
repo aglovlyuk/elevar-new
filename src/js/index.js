69f9bbb2b4bdcfc85d6257d23d5e0085a17a23a2
Glovliuk Oleksii <aglovlyuk@gmail.com>
2020-08-13 00:16:48 +0300

    Initial commit + HP

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+//import 'slick-carousel';
+//import popper from "popper.js";
+//import bootstrap from "bootstrap";
+
+$(function() {
+});
