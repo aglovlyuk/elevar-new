b404d60703f7a7160380afbcb351cb2b6e2d1716
Glovliuk Oleksii <aglovlyuk@gmail.com>
2020-08-20 01:50:06 +0300

    Isotope Search

--- src/js/isotope.js
+++ src/js/isotope.js
@@ -5,6 +5,7 @@ import isotope from 'isotope-layout'
 jQueryBridget( 'isotope', Isotope, $ );
 
 $(window).on('load', function () {
+    let qsRegex;
     const $grid = $('.js-grid-insights').isotope({
         itemSelector: '.grid-item',
         percentPosition: true,
@@ -12,10 +13,13 @@ $(window).on('load', function () {
         masonry: {
             // use outer width of grid-sizer for columnWidth
             columnWidth: '.grid-sizer'
+        },
+        filter: function() {
+            return qsRegex ? $(this).text().match( qsRegex ) : true;
         }
     });
 
-// bind filter button click
+    // bind filter button click
     $('#js-filters')
         .on( 'click', 'button', function() {
             let filterValue = $( this ).attr('data-filter');
@@ -29,4 +33,25 @@ $(window).on('load', function () {
             });
         });
 
+    // use value of search field to filter
+    const $quicksearch = $('.js-filter-search').keyup( debounce( function() {
+        qsRegex = new RegExp( $quicksearch.val(), 'i' );
+        $grid.isotope();
+    }, 200 ) );
+
+    // debounce so filtering doesn't happen every millisecond
+    function debounce( fn, threshold ) {
+        var timeout;
+        threshold = threshold || 100;
+
+        return function debounced() {
+            clearTimeout( timeout );
+            var args = arguments;
+            var _this = this;
+            function delayed() {
+                fn.apply( _this, args );
+            }
+            timeout = setTimeout( delayed, threshold );
+        };
+    }
 });
