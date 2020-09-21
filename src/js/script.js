cb4221fff34da2adde1a50e28df8fb1d85166e6a
Hlovliuk Oleksii <aglovlyuk@gmail.com>
2020-09-22 01:09:38 +0300

    Fixes

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,16 @@ var Application = function(application){
             $(this).hide();
             $(this).parent().find('.hiddenContent').fadeIn();
         });
+
+        // throwing viewport height to CSS var
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+        // We listen to the resize event
+        window.addEventListener('resize', () => {
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        });
     }
 
     return {
